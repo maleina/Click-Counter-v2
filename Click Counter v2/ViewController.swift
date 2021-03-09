3945fabffcf2ca89ecdcf46255149e053aced0af
Maleina Bidek <maleina@ucla.edu>
2021-03-09 09:49:03 -0500

    Initial Commit

---
+++ Click Counter v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click Counter v2
+//
+//  Created by Maleina Bidek on 3/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
